b1c439e4e042f1c2c521c0682895722b4f395eaf
Manjeet Kumar Gautam <86048496+ManjeetKumarGautam@users.noreply.github.com>
2025-01-04 15:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const myName = document.getElementById("name");
+const age = document.getElementById("age");
+const btn = document.getElementById("btn");
+
+// create promise
+function promise1(age) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age < 18) {
+                reject(`Oh sorry ${myName.value}. You aren't old enough.`)
+            }
+            else {
+                resolve(`Welcome, ${myName.value}. You can vote.`)
+            }
+        }, 4000);
+    });
+}
+
+
+btn.addEventListener("click", () => {
+    promise1(age.value).then((res)=>{
+        alert(res);
+    })
+});
